4b92c08b40f8f71909b74195a4e658792d74872f
Narith Choeun <narithchoeun@hotmail.com>
2016-04-25 16:41:39 -0700

    testing mysql

---
+++ src/MimiHospital.sql
@@ -0,0 +1,10 @@
+show databases; -- show all databases
+use cecs323m39; --switch to database
+
+show tables; --show all tables in current database
+CREATE TABLE HospitalPerson (
+    firstName VARCHAR(20),
+    lastName VARCHAR(20)
+);
+
+SELECT * FROM HospitalPerson;
