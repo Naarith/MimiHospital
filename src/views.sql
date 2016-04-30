66edaca20d604b57505ddaaf9c479209b7bed51e
jaylenw <mr2cleanj@live.com>
2016-04-29 19:32:25 -0700

    file for creating the views

---
+++ src/views.sql
@@ -0,0 +1,71 @@
+--###################
+--VIEWS SQL FILE
+
+--Bed
+CREATE VIEW Bed_View AS SELECT * FROM Bed;
+
+--CareCenter
+CREATE VIEW CareCenter_View AS SELECT * FROM CareCenter;
+
+--Employee
+CREATE VIEW Employee_View AS SELECT * FROM Employee;
+
+--HospitalPerson
+CREATE VIEW HospitalPerson_View AS SELECT * FROM HospitalPerson;
+
+--Lab
+CREATE VIEW Lab_View AS SELECT * FROM Lab;
+
+--Nurse
+CREATE VIEW Nurse_View AS SELECT * FROM Nurse;
+
+--Outpatient
+CREATE VIEW Outpatient_View AS SELECT * FROM Outpatient;
+
+--Patient
+CREATE VIEW Patient_View AS SELECT * FROM Patient;
+
+--PersonPhone
+CREATE VIEW PersonPhone_View AS SELECT * FROM PersonPhone;
+
+--Physician
+CREATE VIEW Physician_View AS SELECT * FROM Physician;
+
+--PhysicianSpecialty
+CREATE VIEW PhysicianSpecialty_View AS SELECT * FROM PhysicianSpecialty;
+
+--RN
+CREATE VIEW RN_View AS SELECT * FROM RN;
+
+--Resident
+CREATE VIEW Resident_View AS SELECT * FROM Resident;
+
+--Skill
+CREATE VIEW Skill_View AS SELECT * FROM Skill;
+
+--Specialty
+CREATE VIEW Specialty_View AS SELECT * FROM Specialty;
+
+--Staff
+CREATE VIEW Staff_View AS SELECT * FROM Staff;
+
+--TechLab
+CREATE VIEW TechLab_View AS SELECT * FROM TechLab;
+
+--Technician
+CREATE VIEW Technician_View AS SELECT * FROM Technician;
+
+--TechnicianSkill
+CREATE VIEW TechnicianSkill_View AS SELECT * FROM TechnicianSkill;
+
+--Timecard
+CREATE VIEW Timecard_View AS SELECT * FROM Timecard;
+
+--Visit
+CREATE VIEW Visit_View AS SELECT * FROM Visit;
+
+--Volunteer
+CREATE VIEW Volunteer_View AS SELECT * FROM Volunteer;
+
+--VolunteerSkill
+CREATE VIEW VolunteerSkill_View AS SELECT * FROM VolunteerSkill;
